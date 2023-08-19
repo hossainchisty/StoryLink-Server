1c65a81569ae1a81f77d4e17f2ca3d123f03576a
hossainchisty <hossainchisty@users.noreply.github.com>
2023-08-19 18:30:56 +0000

    style: format files

--- utility/fileManager.js
+++ utility/fileManager.js
@@ -2,12 +2,12 @@
 const fs = require("fs");
 
 const handleFileUpload = (req) => {
-    const { originalname, path } = req.file;
-    const parts = originalname.split(".");
-    const ext = parts[parts.length - 1];
-    const newPath = path + "." + ext;
-    fs.renameSync(path, newPath);
-    return newPath;
+  const { originalname, path } = req.file;
+  const parts = originalname.split(".");
+  const ext = parts[parts.length - 1];
+  const newPath = path + "." + ext;
+  fs.renameSync(path, newPath);
+  return newPath;
 };
 
 module.exports = handleFileUpload;
